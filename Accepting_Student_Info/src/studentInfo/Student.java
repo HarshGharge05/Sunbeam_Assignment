078d44d5d52da588f5d1e0aeedae88ff48f9a6bf
Harsh Gharge <harshgharge03@gmail.com>
2024-06-04 21:43:39 +0530

    some code is added in que2

--- Accepting_Student_Info/src/studentInfo/Student.java
+++ Accepting_Student_Info/src/studentInfo/Student.java
@@ -1,11 +1,38 @@
 package studentInfo;
 
+import java.util.Scanner;
+
 public class Student {
 
-	private int rollnO;
+	private int rollno;
 	private String name;
 	private  int marks;
 	
+	public Student(int rollno, String name, int marks) {
+		this.rollno = rollno;
+		this.name = name;
+		this.marks = marks;
+	}
 	
+	public void acceptStudentDetails() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter your Roll no : ");
+		this.rollno = sc.nextInt();
+		
+		System.out.println("Enter your name : ");
+		this.name = sc.next();
+		
+		System.out.println("Enter your marks : ");
+		this.marks = sc.nextInt();
+	}
+	
+	public void  printStudentDetails() {
+		System.out.println("Your Roll no is : "+rollno);
+		System.out.println("Your name is : "+name);
+		System.out.println("your marks are : "+marks);
+	}
+
+
 	
 }
